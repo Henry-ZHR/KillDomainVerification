f94e3dc18d0e92e18814321e72b9f689dc0a643f
Henry-ZHR <henry-zhr@qq.com>
2022-01-15 17:43:19 +0800

    v2: supports Android 12

--- README.md
+++ README.md
@@ -1,15 +1,23 @@
 # KillDomainVerification
 
-Related: <https://developer.android.com/training/app-links/verify-site-associations>
+Related: <https://developer.android.com/training/app-links/verify-site-associations> <https://developer.android.com/about/versions/12/web-intent-resolution>
 
 This xposed module prevents the system (and also yourself) from allowing apps to always open supported links.
 
-It should support Android 6 - 11, but maybe not Android 12 and above.
+It also enables you to open supported links in apps which are not "verified" on Android 12 and above.
 
 模块功能：打开 http / https 链接时总是询问，而不是直接在 app 中打开
 
-副作用：手动设也没法直接用应用打开链接
+副作用：手动设也没法直接用应用打开链接；Android 12+ 选择时会同时显示未“验证”的 app
 
-支持 Android 6 到 11
+理论支持 Android 6 及以后
 
-![screenshot](screenshot.png)
+## Screenshots
+
+### Android 11 and below
+
+![screenshot1](screenshot1.png)
+
+### Android 12 and above
+
+![screenshot2](screenshot2.png) ![screenshot3](screenshot3.png)
--- app/src/main/java/moe/henry_zhr/kill_domain_verification/MainHook.java
+++ app/src/main/java/moe/henry_zhr/kill_domain_verification/MainHook.java
@@ -1,16 +1,17 @@
 package moe.henry_zhr.kill_domain_verification;
 
+import android.content.Intent;
 import android.os.Build;
 
 import java.lang.reflect.Field;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
-@SuppressWarnings("unused")
 public class MainHook implements IXposedHookLoadPackage {
   private final static String TAG = "KillDomainVerification";
 
@@ -23,39 +24,54 @@ public class MainHook implements IXposedHookLoadPackage {
     XposedBridge.log(String.format("[%s] Starting hook", TAG));
 
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-      XposedBridge.log(String.format(
-          "[%s] Android 12 or above detected, maybe not supported",
-          TAG
-      ));
-    }
+      XposedBridge.log(String.format("[%s] Android 12 or above detected", TAG));
 
-    // https://cs.android.com/android/platform/superproject/+/android-11.0.0_r48:frameworks/base/services/core/java/com/android/server/pm/PackageSettingBase.java
-    XposedHelpers.findAndHookMethod(
-        "com.android.server.pm.PackageSettingBase",
-        lpparam.classLoader,
-        "readUserState",
-        int.class,
-        new XC_MethodHook() {
-          @Override
-          protected void afterHookedMethod(MethodHookParam param) {
-            try {
-              final Field field = XposedHelpers.findField(
-                  param.getResult().getClass(),
-                  "domainVerificationStatus"
-              );
-              if (field.get(param.getResult()).equals(2)) {
-                field.set(param.getResult(), 1);
-              }
-            } catch (Throwable t) {
-              XposedBridge.log(String.format(
-                  "[%s] Cannot set domainVerificationStatus (%s)",
-                  TAG,
-                  t.toString()
-              ));
+      // used in frameworks/base/services/core/java/com/android/server/pm/PackageManagerService.java filterCandidatesWithDomainPreferredActivitiesLPrBody
+      // (cs.android.com failed to get the commit so there isn't a link)
+      XposedHelpers.findAndHookMethod(
+          "com.android.server.pm.verify.domain.DomainVerificationUtils",
+          lpparam.classLoader,
+          "isDomainVerificationIntent",
+          Intent.class,
+          int.class,
+          new XC_MethodReplacement() {
+            @Override
+            protected Object replaceHookedMethod(MethodHookParam param) {
+              return false;
             }
           }
-        }
-    );
+      );
+    } else {
+      XposedBridge.log(String.format("[%s] Android 11 or below detected", TAG));
+
+      // https://cs.android.com/android/platform/superproject/+/android-11.0.0_r48:frameworks/base/services/core/java/com/android/server/pm/PackageSettingBase.java
+      XposedHelpers.findAndHookMethod(
+          "com.android.server.pm.PackageSettingBase",
+          lpparam.classLoader,
+          "readUserState",
+          int.class,
+          new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) {
+              try {
+                final Field field = XposedHelpers.findField(
+                    param.getResult().getClass(),
+                    "domainVerificationStatus"
+                );
+                if (field.get(param.getResult()).equals(2)) {
+                  field.set(param.getResult(), 1);
+                }
+              } catch (Throwable t) {
+                XposedBridge.log(String.format(
+                    "[%s] Cannot set domainVerificationStatus (%s)",
+                    TAG,
+                    t.toString()
+                ));
+              }
+            }
+          }
+      );
+    }
 
     XposedBridge.log(String.format("[%s] Hook finished", TAG));
   }
